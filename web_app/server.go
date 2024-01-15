d9d2b28144acc143f71299a14c484f8f614ba9d2
Tobi Okanlawon <tobiinlondon34@gmail.com>
2024-01-15 22:22:04 +0100

    Add tests and framework for server

--- web_app/server.go
+++ web_app/server.go
@@ -1,17 +1,59 @@
 package web_app
 
 import (
+	"errors"
+	"log"
 	"net/http"
 
 	chi "github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func generateRouter(){
-	r := chi.NewRouter()
-	r.Use(middleware.Logger())
+type RouteURL string
+var ErrorEmptyRouteMap error = errors.New("passed empty RouteMap as arg")
+
+type RouteTuple struct {
+	// TODO: Make this into an enum to mamke it safer
+	Method  string
+	Handler func(w http.ResponseWriter, r *http.Request)
 }
 
-func StartConfigurableWebAppServer(routes map[string][]byte) http.HandlerFunc{
+type RouteMap map[RouteURL][]RouteTuple
+
+func generateRouter(routes RouteMap) *chi.Mux {
+	r := chi.NewRouter()
+	// TODO: the logger should also be injected
+	r.Use(middleware.Logger)
+
+	for url, route_tuple := range routes {
+		for _, tuple  := range route_tuple {
+			r.Method(tuple.Method, string(url), http.HandlerFunc(tuple.Handler))
+		}
+	}
+
+	return r
+}
+
+func StartConfigurableWebAppServer(routes RouteMap) (http.Handler, error) {
+	// r := func (w http.ResponseWriter, r *http.Request) {
+	// w.Write([]byte("Hello, World!"))
+	// }
+
+	if len(routes) == 0 {
+		return nil, ErrorEmptyRouteMap
+	}
 	
+	r := generateRouter(routes)
+
+	return r, nil
+}
+
+func WebAppServer() (http.Handler, error){
+	handler, err := StartConfigurableWebAppServer(make(RouteMap))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return handler, nil
 }
--- web_app/server_test.go
+++ web_app/server_test.go
@@ -7,16 +7,34 @@ import (
 )
 
 func TestRoutes(t *testing.T) {
+	makeDefaultRouteMap := func(t *testing.T, routePath string, routeBody string) RouteMap {
+
+		t.Helper()
+
+		routeMap := make(RouteMap)
+		if routePath == "" {
+			return routeMap
+		}
+
+		if routeBody == "" {
+			return routeMap
+		}
+		
+		routeTuple := RouteTuple{"GET", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(routeBody))
+		}}
+		routeMap[RouteURL(routePath)] = []RouteTuple{routeTuple}
+		return routeMap
+	}
 	t.Run("returns the page at /", func(t *testing.T) {
-		routes := make(map[string][]byte)
 		homeRouteBody := "Hello, World!"
-		routes["/"] = []byte(homeRouteBody)
+		routes := makeDefaultRouteMap(t, "/", homeRouteBody)
 		request, _ := http.NewRequest(http.MethodGet, "/", nil)
 		response := httptest.NewRecorder()
 
-		handler := StartConfigurableWebAppServer(routes)
-		
-		handler(response, request)
+		handler, _ := StartConfigurableWebAppServer(routes)
+
+		handler.ServeHTTP(response, request)
 
 		got := response.Body.String()
 		want := homeRouteBody
@@ -24,6 +42,31 @@ func TestRoutes(t *testing.T) {
 		if got != want {
 			t.Errorf("want %s, but got %s", want, got)
 		}
-		
+
+	})
+
+	t.Run("returns a 404 for pages that don't exist", func(t *testing.T) {
+		homeRouteBody := "Hello, World!"
+		routes := makeDefaultRouteMap(t, "/", homeRouteBody)
+		request, _ := http.NewRequest(http.MethodGet, "/does/not/exist", nil)
+		response := httptest.NewRecorder()
+
+		handler, _ := StartConfigurableWebAppServer(routes)
+		handler.ServeHTTP(response, request)
+
+		got := response.Result().StatusCode
+		want := 404
+
+		if got != want {
+			t.Errorf("doesn't return 404 on non-existent route, returns %d", got)
+		}
+	})
+
+	t.Run("returns an error on empty RouteMaps", func(t *testing.T) {
+		routeMap := makeDefaultRouteMap(t, "", "")
+		_, err := StartConfigurableWebAppServer(routeMap)
+		if err != ErrorEmptyRouteMap {
+			t.Fatal("expected an error to be generated while passing an empty routeMap")
+		}
 	})
 }
